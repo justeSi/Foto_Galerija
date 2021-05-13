936b4b680725d4d2b0749e261c2fa0b1b7be8036
justeSi <69959214+justeSi@users.noreply.github.com>
2021-05-13 18:53:41 +0300

    *pertvarkytas darbas be jQuery

--- app.js
+++ app.js
@@ -1,42 +1,103 @@
-
-// -----------------API----------------------------------
 var row = document.querySelector('.row');
-fetch('https://api.unsplash.com/photos?client_id=_CfdRIyvRxD4LnrNlRj-r7pGE5ZAcul5s_Pp4Vy41x0&page=55')
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-    for(var i = 0; i < data.length; i++) {
-        console.log(data[i].urls.regular);
-        row.innerHTML+= 
-        `<div class="col-md-6 grid-item">
-                <img class=" image"  src="${data[i].urls.regular}" alt="">
-                    <h5 class="name mt-3">Fotografas: ${data[i].user.first_name}</h5>
-                    <p class="title">Aprašymas: ${data[i].alt_description}</p>
-                    <p class="likes"><i class="fa fa-thumbs-up"></i> ${data[i].likes}</p>
-                </div>`    
-            }
-            // ------------------------RIKIAVIMAS----------------------------------
-            var iso = new Isotope('.grid', {
-                itemSelector: '.grid-item',
-                stagger: 30,
-                getSortData: {
-                    name: '.name',
-                    title: '.title',
-                    number: '.likes parseInt'
-                },
-                masonry: {
-                    columnWidth: '.grid-sizer',
-                    gutter: '.grid-gutter-sizer'
-                },
-                sortBy: 'original-order',
-                sortAscending: {
-                    name: true,
-                    number: true
+var iso;
+const accesssKey = ''; //Kas norit išbandyti prašom naudoti nuosavą access key 
+pages = 1;
+// 
+loadImages(pages);
+       
+//      Duomenų paėmimas iš API
+
+        function loadImages(pages){
+            fetch("https://api.unsplash.com/photos?client_id=" + accesssKey + "&page="+ pages)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+        
+                    for(var i = 0; i < data.length; i++ ) {
+                        // Paiimamas iš JSON nuotraukos autoriaus vardas
+                        var fElement = document.createElement('h5');
+                        fElement.classList.add('name', 'mt-3');
+
+
+                        // patikrinimas ar duomenys ne tušti
+                        if (!data[i].user.first_name){
+                            fElement.innerText = 'Autorius nežinomas';
+                        }else {
+                            fElement.innerText = ('Fotografas: ' + data[i].user.first_name);
+                        }
+
+
+                        // Paiimamas iš JSON nuotraukos apbibūdinimas
+                        var pElement = document.createElement('p');
+                        pElement.classList.add('title');
+                        if (!data[i].alt_description){
+                            pElement.innerText = 'apibūdinimo nėra';
+                        }else {
+                            pElement.innerText = data[i].alt_description;
+                        }
+
+
+                        // Paiimamas iš JSON "patiktukai"
+                        var likeElement = document.createElement('p');
+                        likeElement.classList.add('likes');
+                        
+                            if (!data[i].likes){
+                                likeElement.innerText = 'Už šią nuotrauką niekas nebalsavo';
+                            }else {
+                                
+                                likeElement.innerText =  data[i].likes;
+                        }
+    
+                        // console.log(data[i].urls.regular);
+
+                        // susikuriame Img
+                        var imageElement = document.createElement('img');
+                        imageElement.src = data[i].urls.regular;
+
+                        // susikuriame div
+                        var divElement = document.createElement('div');
+                        divElement.classList.add('col-md-6');
+                        divElement.classList.add('grid-item');
+
+                        // prie div prikabiname img, autorių, apibūdinimą ir populiarumą
+    
+                        divElement.append(imageElement);
+                        divElement.append(fElement);
+                        divElement.append(pElement);
+                        divElement.append(likeElement);
+    
+                        document.querySelector('.grid').append(divElement);
+                    }
+                    iso = new Isotope('.grid', {
+                        itemSelector: '.grid-item',
+                        stagger: 30,
+                        getSortData: {
+                            name: '.name',
+                            title: '.title',
+                            number: '.likes parseInt' //perseInt vertimas string į integer
+                        },
+                        masonry: {
+                            columnWidth: '.grid-sizer',
+                            gutter: '.grid-gutter-sizer'
+                        },
+                        sortBy: 'original-order',
+                        sortAscending: {
+                            name: true,
+                            number: true
+                        }
+                    });
                 }
-            });
+            );
+
+        };
+
+
+            //                  RIKIAVIMAS
+           
             var sortByGroup = document.querySelector('.sort-by-button-group');
                 sortByGroup.addEventListener('click', function(event){
-
+                    
+                       // Funcijos nutraukimas jei fokusuojamas ne mygtuko elementas
                     if (!matchesSelector(event.target, '.btn')) {
                         return;
                     }
@@ -56,95 +117,24 @@ fetch('https://api.unsplash.com/photos?client_id=_CfdRIyvRxD4LnrNlRj-r7pGE5ZAcul
                     console.log(sortValue);
                     console.log( sortDirection);
                 });
-        });
+        // });
             
-// -----------------INFINETE SCROLL.......................................
-        var currentscrollHeight = 0;
-        var count = 0;
-        // Parenkamas skaičius puslapio nuo kurio vis scrollinant keisis 'page' nr
-        var sig = 20;
-        ScrollDebounce = true;
-        
-    $(window).on("scroll", function () {
-        const scrollHeight = $(document).height();
-        const scrollPos = Math.floor($(window).height() + $(window).scrollTop());
-        const isBottom = scrollHeight - 100 < scrollPos;
-         
-        if (isBottom && currentscrollHeight < scrollHeight) { 
-        
-            if (ScrollDebounce) { 
-                ScrollDebounce = false;
-            
-                fetch('https://api.unsplash.com/photos?client_id=_CfdRIyvRxD4LnrNlRj-r7pGE5ZAcul5s_Pp4Vy41x0&page='+sig)
-                .then(res => res.json())
-                .then(data => {
-
-                for (var i = 0; i < data.length; i++) {
-                    console.log(data[i].urls.regular);
-                    row.innerHTML+= `
-                    <div class="col-md-6 grid-item">
-                            <img class=" image"  src="${data[i].urls.regular}" alt="">
-                            <h5 class="name mt-3">Fotografas: ${data[i].user.first_name}</h5>
-                            <p class="title">Aprašymas: ${data[i].alt_description}</p>
-                            <p class="likes"><i class="fa fa-thumbs-up"></i> ${data[i].likes}</p>
-                    </div>`    
-                        }
-                        sig++;
-            console.log(sig);
-                        var iso = new Isotope('.grid', {
-                            itemSelector: '.grid-item',
-                            stagger: 30,
-                            getSortData: {
-                                name: '.name',
-                                title: '.title',
-                                number: '.likes parseInt'
-                            },
-                            masonry: {
-                                columnWidth: '.grid-sizer',
-                                gutter: '.grid-gutter-sizer'
-                            },
-                            sortBy: 'title',
-                            sortAscending: {
-                                name: true,
-                                number: true
-                            }
-                        });
-                        var sortByGroup = document.querySelector('.sort-by-button-group');
-                sortByGroup.addEventListener('click', function(event){
-
-                    if (!matchesSelector(event.target, '.btn')) {
-                        return;
-                    }
-
-                    var sortValue = event.target.getAttribute('data-sort-value');
-                    var sortDirection = event.target.getAttribute('data-sort-direction');
-                    var isAscending = (sortDirection == 'asc'); //true, folse
-
-                    iso.arrange({
-                        sortBy: sortValue,
-                        sortAscending: isAscending
-                    });
-
-
-                    // console.log(isAscending);
-
-                    // console.log(sortValue);
-                    // console.log( sortDirection);
-                });
-            
-            });
-            console.log(ScrollDebounce);
-            setTimeout(function () { ScrollDebounce = true; }, 100); 
-            currentscrollHeight = scrollHeight;
-        
-    }
+//                   INFINETE SCROLL
+    window.addEventListener('scroll', function(){
+        var pozicija = Math.floor(window.scrollY + window.innerHeight);
+        if (pozicija >= document.body.offsetHeight) {
+                console.log('psl pabaiga');
+                pages++;
+                console.log(pages);
+                loadImages(pages);
     }});
-    // ======="Lengas scrollinimas atgal į viršų =========="
-    $('#up').click(function() {
-        window.scroll({
+
+    // "Lenvgas" grįžimas atgal į viršų 
+    var goTop = document.querySelector('#up');
+    goTop.addEventListener('click', () => window.scrollTo({
             top: 0, 
             left: 0, 
             behavior: 'smooth'
-          });
-    });
+        }));
+    
     
